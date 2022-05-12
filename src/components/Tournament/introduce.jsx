6741de7a89e20d9c14a76f9861b2cca028df4a32
黄田 <huangtian@hengtaont.com>
2022-05-12 14:26:03 +0800

    比赛流程

--- src/components/Tournament/introduce.jsx
+++ src/components/Tournament/introduce.jsx
@@ -29,6 +29,142 @@ export default function Introduce() {
           </div>
         </div>
       </div>
+
+      <div className="match white-bg">
+        <div className="left">
+          <div className="match-text">比赛流程</div>
+          <div className="left-round">
+            <div className="round">
+              <div className="time-wrapper">
+                <div className="month">6.6</div>
+                <div className="year">2022</div>
+              </div>
+            </div>
+            <div className="left-line"></div>
+            <div className="icon-wrapper">
+              <Image
+                src="/tournament/Group 142.png"
+                style={{ width: "24px", height: "24px" }}
+              />
+            </div>
+          </div>
+
+          <div className="match-end">
+            <div className="match-end-text">初赛结束</div>
+            <div className="match-end-content">
+              <div>第一轮：指标分数（权重60%）</div>
+              <div>综合分数 = 0.6*年化收益率（标准化后）+ 0.4*夏普比例</div>
+              <div>（标准化后）</div>
+            </div>
+          </div>
+
+          <div className="flex-end" style={{ marginTop: "145px" }}>
+            <div className="month">9.7</div>
+            <div className="right-line bg"></div>
+            <div className="icon-wrapper">
+              <Image
+                src="/tournament/Group 142.png"
+                style={{ width: "24px", height: "24px" }}
+              />
+            </div>
+          </div>
+
+          <div className="match-end" style={{ marginTop: "168px" }}>
+            <div className="match-end-text">投资报告提交截止</div>
+          </div>
+
+          <div className="flex-end" style={{ marginTop: "143px" }}>
+            <div className="month">9.12</div>
+            <div className="right-line bg"></div>
+            <div className="icon-wrapper">
+              <Image
+                src="/tournament/Group 142.png"
+                style={{ width: "24px", height: "24px" }}
+              />
+            </div>
+          </div>
+
+          <div className="match-end" style={{marginTop:"161px"}}>
+            <div className="match-end-text">决赛（全网直播）</div>
+            <div className="match-end-content">
+              <div>
+                决赛选手将进行10-20分钟线上投资报告展示（具体展示模版将在初赛结束后公布），评委对其专业能力，以及综合素质进行打分。最终将结合初赛的指标分数（权重40%）以及决赛评委分数（权重60%）作出最终排名。
+              </div>
+            </div>
+          </div>
+        </div>
+
+        <div className="right">
+          <div className="match-begin">
+            <div className="right-line"></div>
+            <div className="match-begin-text">比赛开始</div>
+          </div>
+
+          <div className="flex-start" style={{ marginTop: "247px" }}>
+            <div className="icon-wrapper-left">
+              <Image
+                src="/tournament/Group 142.png"
+                style={{ width: "24px", height: "24px" }}
+              />
+            </div>
+            <div className="right-line bg"></div>
+            <div className="month" style={{ paddingLeft: "16px" }}>
+              9.2
+            </div>
+          </div>
+
+          <div className="finals-name">
+            <div className="right-line"></div>
+            <div className="finals-name-container">
+              <div className="match-end-text" style={{ textAlign: "left" }}>
+                公布决赛名单
+              </div>
+              <div className="match-end-content" style={{ textAlign: "left" }}>
+                <div>第二轮：投资报告（权重40%）</div>
+                <div>
+                  由综合分数排名前100名的选手提交一份逻辑报告。UFA评委综合逻辑报告，以及各项指标筛选出15名入围决赛选手。
+                </div>
+              </div>
+            </div>
+          </div>
+
+          <div className="flex-start" style={{ marginTop: "120px" }}>
+            <div className="icon-wrapper-left">
+              <Image
+                src="/tournament/Group 142.png"
+                style={{ width: "24px", height: "24px" }}
+              />
+            </div>
+            <div className="right-line bg"></div>
+            <div className="month" style={{ paddingLeft: "16px" }}>
+              9.10
+            </div>
+          </div>
+
+          <div className="flex-start" style={{ marginTop: "143px" }}>
+            <div className="right-line"></div>
+            <div className="match-begin-text">UFA评委评选决赛</div>
+          </div>
+
+          <div className="flex-start" style={{marginTop:"143px"}}>
+            <div className="icon-wrapper-left">
+              <Image
+                src="/tournament/Group 142.png"
+                style={{ width: "24px", height: "24px" }}
+              />
+            </div>
+            <div className="right-line bg"></div>
+            <div className="left-round" style={{ marginTop: "0px" }}>
+              <div className="round">
+                <div className="time-wrapper" style={{padding:"80px 20px"}}>
+                  <div className="month">9.17</div>
+                  <div className="year">2022</div>
+                </div>
+              </div>
+            </div>
+          </div>
+        </div>
+      </div>
     </div>
   );
 }
