90e9b76d9b7ed66771ccd7bdc1a396c488baac66
xukun <1196412004@qq.com>
2022-05-19 10:57:27 +0800

    个股交易接口对接

--- src/components/screen/KeyIndicatorProfessional.jsx
+++ src/components/screen/KeyIndicatorProfessional.jsx
@@ -1,7 +1,28 @@
-import data from '../../static/indicator.json'
+import React, { useEffect,useState } from 'react';
+import axios from 'axios';
 import './KeyIndicator.css'
 
 export default function KeyIndicators(){
+	const [data, setData] = useState('')
+	const [extend, setExtend] = useState(true)
+	
+	useEffect(()=>{
+		if(extend){
+			getIndicator()
+		}
+	})
+	
+	const getIndicator= () => {
+	    return axios.get('http://82.157.18.223:10987/api/live/ticker?symbol=SH.000001')
+		.then(function (response) {
+	        console.log(response.data.data)
+			setData(response.data.data)
+			setExtend(false)
+	      })
+	      .catch(function (error) {
+	        console.log(error);
+	      });
+	}
 	return(
 		<>
 			<div className="indicator-professional">
