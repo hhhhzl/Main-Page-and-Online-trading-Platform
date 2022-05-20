8d27ba5ba6f64ab14b87e54597b003c1816d378f
Yuxuan Fan <39680169+991231@users.noreply.github.com>
2022-05-20 16:29:04 -0700

    fix user_messages urls

--- src/api/main_platform/user_messages.js
+++ src/api/main_platform/user_messages.js
@@ -5,14 +5,14 @@ import apiService from "./apiService";
 // query_params: N/A
 // note: recipients - an array of user ids, or all users if it's empty
 export const apiGetAllAdminMessages = (query_params) =>
-    apiService.get(`users/get_admin_message/`, { params: query_params });
+    apiService.get(`user_messages/get_admin_message/`, { params: query_params });
 export const apiGetAdminMessage = (id) =>
-    apiService.get(`users/get_admin_message/${id}/`);
+    apiService.get(`user_messages/get_admin_message/${id}/`);
 export const apiGetAdminMessageRecipient = (id) =>
-    apiService.get(`users/get_admin_message_recipient/${id}/`);
+    apiService.get(`user_messages/get_admin_message_recipient/${id}/`);
 export const apiCreateAdminMessage = (data) =>
-    apiService.post(`users/create_admin_message/`, data);
+    apiService.post(`user_messages/create_admin_message/`, data);
 export const apiReadAdminMessage = (id) =>
-    apiService.post(`users/read_admin_message/${id}/`);
+    apiService.post(`user_messages/read_admin_message/${id}/`);
 export const apiDeleteAdminMessage = (id) =>
-    apiService.post(`users/delete_admin_message/${id}/`);
+    apiService.post(`user_messages/delete_admin_message/${id}/`);
