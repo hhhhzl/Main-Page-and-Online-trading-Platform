5ac18d92bb21010c4d6da79aca44ea8526c4e8dc
Yuxuan Fan <39680169+991231@users.noreply.github.com>
2022-05-20 12:37:35 -0700

    setup axios api functions

---
+++ src/api/trading_platform/trade.js
@@ -0,0 +1,19 @@
+import apiService from "./apiService";
+
+// stats
+export const apiRank = ({ APIKEY, page, limit }) =>
+    apiService.post(`/stats/rank`, { APIKEY, page, limit });
+
+// trade
+export const apiOrders = ({ API_KEY, type }) =>
+    apiService.get(`/trade/orders`, { params: { API_KEY, type } });
+export const apiCancelOrder = ({ API_KEY, order_id }) =>
+    apiService.post(`/trade/cancel_order`, { API_KEY, order_id });
+export const apiMakeOrder = ({ API_KEY, symbol, side, amount, order_price }) =>
+    apiService.post(`/trade/make_order`, { API_KEY, symbol, side, amount, order_price });
+
+// account
+export const apiAccountPositions = ({ API_KEY }) =>
+    apiService.get(`/trade/orders`, { params: { API_KEY } });
+export const apiAccountCash = ({ API_KEY }) =>
+    apiService.get(`/trade/cash`, { params: { API_KEY } });
