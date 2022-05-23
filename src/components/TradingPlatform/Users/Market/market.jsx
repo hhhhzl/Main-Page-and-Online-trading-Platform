c13506c132586c3ab497f77704ddcc25db99367c
hhhhzl <hhhhzl@bu.edu>
2022-05-22 22:05:55 -0700

    change

--- src/components/TradingPlatform/Users/Market/market.jsx
+++ src/components/TradingPlatform/Users/Market/market.jsx
@@ -11,12 +11,14 @@ import AreaChart from '../../../graphs/areaChart';
 import MarketOverview from '../../../screen/MarketOverView';
 import { flexbox } from '@material-ui/system';
 import LeadingIndustry from '../../../screen/LeadingIndustry';
+import LeadingIndustryAll from '../../../screen/AllIndustry'
 import PageHeader from '../../../screen/PageHeader';
 import MarketUpdownDistribute from '../../../screen/MarkeUpdownDistribute';
 
 export default function UserMarket() {
     const {width,height} = useWindowDimensions();
     const [extend, setExtend] = useState(true)
+    const [switchClose, setswitchClose] = useState(true)
 
     const extendbar = () => {
         setExtend(!extend)
@@ -25,31 +27,52 @@ export default function UserMarket() {
         
     <> 
     <PageHeader/>
-    {width>700? <>
-        <div style={{width:"100%",minWidth:"920px",minHeight:"300px",display:"flex",justifyContent:"space-between"}}>
-        <div style={{width:"50%", height:height*0.52}}>
+    <Card style={{padding:0, borderColor:"white"}}>
+        <Collapse in={switchClose}>
+            <Card.Body style={{padding:0, borderColor:"white"}}>
+    {width>700?
+    <>
+        <div style={{width:"100%",minWidth:"920px",minHeight:"500px",display:"flex",justifyContent:"space-between"}}>
+        
+        <div style={{width:"50%", height:height*0.55,minHeight:"500px"}}>
             <MarketOverview widthRatio = {width>920? width/2 : 920/2}/>
         </div>
-        <div style={{width:"1px",height:height*0.52,backgroundColor:"#E5E8EE"}}></div>
-        <div style={{width:"50%",height:height*0.52}}>
+        <div style={{width:"1px",height:height*0.55,backgroundColor:"#E5E8EE"}}></div>
+        <div style={{width:"50%",height:height*0.52, minHeight:"500px"}}>
             <MarketUpdownDistribute widthRatio = {width>920? width/2 : 920/2}/>
         </div> 
+        
     </div>
-    </> : <>
-        <div style={{width:"100%", height:height*0.52, minHeight:"300px"}}>
+    
+    </>
+     : <>
+        <div style={{width:"100%", height:height*0.56, minHeight:"300px"}}>
             <MarketOverview widthRatio = {width}/>
         </div>
-        <div style={{width:"100%",height:height*0.52}}>
+        <div style={{width:"100%",height:height*0.56}}>
             <MarketUpdownDistribute widthRatio = {width}/>
         </div> 
     
     </>}
-   
+    </Card.Body>
+    </Collapse>
+    </Card>
 
-    <div style ={{marginTop:height*0.056,margin:"48px"}}>
-        <LeadingIndustry/>
+    {switchClose? <>
+        <div style ={{marginTop:height*0.056,margin:"48px"}}>
+        <LeadingIndustry setswitchClose = {setswitchClose}/>
 
     </div>
+    </>
+    :
+    <>
+    <LeadingIndustryAll setswitchClose= {setswitchClose}/>
+    </>
+    }
+    
+   
+
+    
 
     </>
 
