704fc046f15ffea97814474969b617ea93f15454
黄田 <huangtian@hengtaont.com>
2022-05-30 09:49:34 +0800

    活动图集

--- src/components/webpage/previousReview/previousreview.jsx
+++ src/components/webpage/previousReview/previousreview.jsx
@@ -16,6 +16,7 @@ const Review = ({ projectImgs }) => {
   const [type, setType] = useState("front-end");
   const [image, setimage] = useState(false);
   const [video, setvideo] = useState(true);
+  // const data = [];
 
   const Openvideo = () => {
     setvideo(true);
@@ -61,7 +62,7 @@ const Review = ({ projectImgs }) => {
                 margin: "0px",
                 fontSize: "14px",
                 fontWeight: "bold",
-                fontFamily: 'Microsoft YaHei UI-Regular, Microsoft YaHei UI',
+                fontFamily: "Microsoft YaHei UI-Regular, Microsoft YaHei UI",
                 lineHeight: "24px",
                 color: video ? "#FFFFFF" : "#9C9EAC",
                 fontWeight: video ? "bold" : "400",
@@ -89,7 +90,7 @@ const Review = ({ projectImgs }) => {
                 margin: "0px",
                 fontSize: "14px",
                 fontWeight: "bold",
-                fontFamily: 'Microsoft YaHei UI-Regular, Microsoft YaHei UI',
+                fontFamily: "Microsoft YaHei UI-Regular, Microsoft YaHei UI",
                 lineHeight: "24px",
                 color: image ? "#FFFFFF" : "#9C9EAC",
                 fontWeight: image ? "bold" : "400",
@@ -101,19 +102,25 @@ const Review = ({ projectImgs }) => {
         </div>
       </div>
 
-      <div className="video-container" style={{position: "relative"}}>
+      <div className="video-container" style={{ position: "relative" }}>
         <div className="review-icon-wrapper">
           <div className="review-icon">
-            {width > 700 ? <Image
-              src="/homeCutout/Group 143.png"
-              style={{ width: "108px", height: "108px" }}
-            /> : <><br/><br/></> }
-
+            {width > 700 ? (
+              <Image
+                src="/homeCutout/Group 143.png"
+                style={{ width: "108px", height: "108px" }}
+              />
+            ) : (
+              <>
+                <br />
+                <br />
+              </>
+            )}
           </div>
         </div>
-        <div style={{ minHeight: "150px" }} >
-          <Collapse in={video} dimension="width" >
-            <div id="example-collapse-text"  >
+        <div style={{ minHeight: "150px" }}>
+          <Collapse in={video} dimension="width">
+            <div id="example-collapse-text">
               <BilibiliVideo
                 cid="548193318"
                 aid="382170523"
@@ -125,56 +132,48 @@ const Review = ({ projectImgs }) => {
           </Collapse>
           <Collapse in={image} dimension="width">
             <div id="example-collapse-text" className="image-banner">
-                <Carousel fade >
-                    <Carousel.Item>
-                        <img
-                            style={{objectFit: 'cover'}}
-                            className="image-banner"
-                            src="https://gimg2.baidu.com/image_search/src=http%3A%2F%2Fimg.jj20.com%2Fup%2Fallimg%2F1114%2F113020142315%2F201130142315-1-1200.jpg&refer=http%3A%2F%2Fimg.jj20.com&app=2002&size=f9999,10000&q=a80&n=0&g=0n&fmt=auto?sec=1656213996&t=acdd17c2d37e0529c42e5552e73fa36b"
-                            alt="First slide"
-                        />
-                        {/*<Carousel.Caption>*/}
-                        {/*    <h3>First slide label</h3>*/}
-                        {/*    <p>Nulla vitae elit libero, a pharetra augue mollis interdum.</p>*/}
-                        {/*</Carousel.Caption>*/}
-                    </Carousel.Item>
-                    <Carousel.Item>
-                        <img
-                            style={{objectFit: 'cover'}}
-                            className="image-banner"
-                            src="https://gimg2.baidu.com/image_search/src=http%3A%2F%2Fimg.jj20.com%2Fup%2Fallimg%2F4k%2Fs%2F02%2F2109242306111155-0-lp.jpg&refer=http%3A%2F%2Fimg.jj20.com&app=2002&size=f9999,10000&q=a80&n=0&g=0n&fmt=auto?sec=1656213996&t=1902d4a73339dc2ea9971999571338c7"
-                            alt="Second slide"
-                        />
+              <Carousel fade>
+                {/* {data.map((item, idx) => (
+                  <Carousel.Item>
 
-                        {/*<Carousel.Caption>*/}
-                        {/*    <h3>Second slide label</h3>*/}
-                        {/*    <p>Lorem ipsum dolor sit amet, consectetur adipiscing elit.</p>*/}
-                        {/*</Carousel.Caption>*/}
-                    </Carousel.Item>
-                    <Carousel.Item>
-                        <img
-                            style={{objectFit: 'cover'}}
-                            className="image-banner"
-                            src="https://gimg2.baidu.com/image_search/src=http%3A%2F%2Fimg.jj20.com%2Fup%2Fallimg%2F1114%2F063021120F9%2F210630120F9-1-1200.jpg&refer=http%3A%2F%2Fimg.jj20.com&app=2002&size=f9999,10000&q=a80&n=0&g=0n&fmt=auto?sec=1656213996&t=3f64664062d613b473c5727310605d13"
-                            alt="Third slide"
-                        />
-
-                        {/*<Carousel.Caption>*/}
-                        {/*    <h3>Third slide label</h3>*/}
-                        {/*    <p>Praesent commodo cursus magna, vel scelerisque nisl consectetur.</p>*/}
-                        {/*</Carousel.Caption>*/}
-                    </Carousel.Item>
-                </Carousel>
+                  </Carousel.Item>
+                ))} */}
+                <Carousel.Item>
+                  <img
+                    style={{ objectFit: "cover" }}
+                    className="image-banner"
+                    src="https://gimg2.baidu.com/image_search/src=http%3A%2F%2Fimg.jj20.com%2Fup%2Fallimg%2F1114%2F113020142315%2F201130142315-1-1200.jpg&refer=http%3A%2F%2Fimg.jj20.com&app=2002&size=f9999,10000&q=a80&n=0&g=0n&fmt=auto?sec=1656213996&t=acdd17c2d37e0529c42e5552e73fa36b"
+                    alt="First slide"
+                  />
+                </Carousel.Item>
+                <Carousel.Item>
+                  <img
+                    style={{ objectFit: "cover" }}
+                    className="image-banner"
+                    src="https://gimg2.baidu.com/image_search/src=http%3A%2F%2Fimg.jj20.com%2Fup%2Fallimg%2F4k%2Fs%2F02%2F2109242306111155-0-lp.jpg&refer=http%3A%2F%2Fimg.jj20.com&app=2002&size=f9999,10000&q=a80&n=0&g=0n&fmt=auto?sec=1656213996&t=1902d4a73339dc2ea9971999571338c7"
+                    alt="Second slide"
+                  />
+                </Carousel.Item>
+                <Carousel.Item>
+                  <img
+                    style={{ objectFit: "cover" }}
+                    className="image-banner"
+                    src="https://gimg2.baidu.com/image_search/src=http%3A%2F%2Fimg.jj20.com%2Fup%2Fallimg%2F1114%2F063021120F9%2F210630120F9-1-1200.jpg&refer=http%3A%2F%2Fimg.jj20.com&app=2002&size=f9999,10000&q=a80&n=0&g=0n&fmt=auto?sec=1656213996&t=3f64664062d613b473c5727310605d13"
+                    alt="Third slide"
+                  />
+                </Carousel.Item>
+              </Carousel>
             </div>
           </Collapse>
         </div>
         <div className="review-icon-wrapper-bottom">
           <div className="review-icon-bottom">
-            {width>700? <Image
-              src="/homeCutout/Group 27.png"
-              style={{ width: "108px", height: "108px" }}
-            /> :null}
-
+            {width > 700 ? (
+              <Image
+                src="/homeCutout/Group 27.png"
+                style={{ width: "108px", height: "108px" }}
+              />
+            ) : null}
           </div>
         </div>
         <br />
