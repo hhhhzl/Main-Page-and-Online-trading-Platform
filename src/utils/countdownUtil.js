d560851dc4616bab94479ef9c66700a8f29997cc
hhhhzl <hhhhzl@bu.edu>
2022-05-30 16:01:42 -0700

    change

--- src/utils/countdownUtil.js
+++ src/utils/countdownUtil.js
@@ -2,7 +2,7 @@
 // import "moment";
 
 export const now = Math.round(new Date().getTime()).toString(); //获取当前时间
-export const end = new Date("2022-06-14 00:00:00").getTime(); //设置截止时间
+export const end = new Date("2022-06-14 00:00:00").getTime(); //设置截止时间  UTC-0     ///UTC + 8
 export const timeDifference = (() =>{
   return end - now
 })
